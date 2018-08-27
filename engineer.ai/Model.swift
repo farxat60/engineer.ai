cae49f78666ba580e65eb7c6c044ea5a921e8098
farxat60@gmail.com <farxat60@gmail.com>
2018-08-27 15:16:06 +0300

    create project

---
+++ engineer.ai/Model.swift
@@ -0,0 +1,9 @@
+//
+//  Model.swift
+//  engineer.ai
+//
+//  Created by Joe Franc on 8/27/18.
+//  Copyright © 2018 farxat60. All rights reserved.
+//
+
+import Foundation
---
+++ engineer.ai/TableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  TableViewCell.swift
+//  engineer.ai
+//
+//  Created by Joe Franc on 8/27/18.
+//  Copyright © 2018 farxat60. All rights reserved.
+//
+
+import UIKit
+
+class TableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
